62b94195b81b75f0f79d169f30e8063a0225a498
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-09-18 22:51:16 +0500

    make gilroy

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -28,7 +28,7 @@
         @click="(e) => click(e)"
         :class="[
             colorScheme === 'light' &&  (userStore.colorScheme === 'light' ? 'bg-[#D0D9FA] text-[#1B1C37]' : 'bg-[#262744] text-[#FFFFFF]'),
-            'font-medium text-center cursor-pointer text-[16px] flex justify-center items-center h-[50px] rounded-[6px] w-full max-w-lg px-2 active:opacity-75'
+            'font-gilroy font-medium text-center cursor-pointer text-[16px] flex justify-center items-center h-[50px] rounded-[6px] w-full max-w-lg px-2 active:opacity-75'
         ]"
     >
         {{ title }}
