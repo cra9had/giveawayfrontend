2799dae1011c8daaf302b50a3f1f7db3d241a767
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-09-19 15:17:26 +0500

    add copy button

--- src/components/Hero/ShareDialog.vue
+++ src/components/Hero/ShareDialog.vue
@@ -8,8 +8,11 @@
     ]"
     class="p-2 flex flex-col justify-center rounded-t-lg w-full max-h-[90vh] overflow-y-auto text-center"
   >
-    <div class="m-l-auto flex flex-col justify-start gap-2 items-start">
-      <div @click="closeDialog" class="reject cursor-pointer font-gilroy text-[14px] font-normal text-orange_color">
+    <div class="m-l-auto flex flex-col justify-start gap-4 items-start">
+      <div
+        @click="closeDialog"
+        class="reject cursor-pointer font-gilroy text-[14px] font-normal text-orange_color"
+      >
         Отмена
       </div>
       <div
@@ -42,34 +45,106 @@
           дополнительный билет для розыгрыша.
         </p>
       </div>
-      <p
-          :class="[
-      ' text-[18px] font-normal font-gilroy',
-      userStore.colorScheme === 'light'
-        ? 'text-primary_dark_color'
-        : 'text-text_light_color',
-    ]"
-      >
-        Ваша реферальная ссылка:
-      </p>
-      <div
-          class="dialog_card w-full rounded-[6px] p-2 text-left flex flex-col gap-2"
-          :class="[
+     <main class="m-l-auto flex flex-col justify-start gap-2 items-start w-full">
+       <p
+           :class="[
+          ' text-[18px] font-normal font-gilroy',
+          userStore.colorScheme === 'light'
+            ? 'text-primary_dark_color'
+            : 'text-text_light_color',
+        ]"
+       >
+         Ваша реферальная ссылка:
+       </p>
+       <div
+           class="dialog_card w-full rounded-[6px] p-2 text-left flex flex-col gap-2"
+           :class="[
           userStore.colorScheme === 'light' ? 'bg-[#fff]' : 'bg-[#32334B]',
         ]"
-      >
+       >
+         <input
+             type="text"
+             v-model="link"
+             readonly
+             class="copy-input p-2 font-medium font-gilroy outline-none rounded-[6px] h-[50px]"
+             :class="[
+            userStore.colorScheme === 'light'
+              ? 'bg-[#D0D9FA] text-primary_dark_color'
+              : 'bg-primary_dark_color text-[#fff]',
+          ]"
+         />
+         <div class="grid grid-cols-2 gap-2">
+           <Button @click="copy" colorScheme="orange" title="Скопировать" />
+           <Button colorScheme="orange" title="Поделиться" />
+         </div>
+       </div>
+     </main>
 
-        <input type="text" value="https://t.me/LotoClubBot/app?startapp=L2cb4c0" readonly class="copy-input p-2 font-medium font-gilroy   outline-none rounded-[6px] h-[50px]"
-               :class="[
-          userStore.colorScheme === 'light' ? 'bg-[#D0D9FA] text-primary_dark_color' : 'bg-primary_dark_color text-[#fff]',
+     <main class="m-l-auto flex flex-col justify-start gap-2 items-start w-full">
+       <p
+           :class="[
+          ' text-[18px] font-normal font-gilroy',
+          userStore.colorScheme === 'light'
+            ? 'text-primary_dark_color'
+            : 'text-text_light_color',
         ]"
-        >
-<div class="grid grid-cols-2 gap-2">
-
-  <Button colorScheme="orange" title="Скопировать" />
-  <Button colorScheme="orange" title="Поделиться" />
-</div>
-      </div>
+       >
+         Статистика:
+       </p>
+       <div
+           class="dialog_card w-full rounded-[6px] p-2 text-left flex flex-col gap-2"
+           :class="[
+          userStore.colorScheme === 'light' ? 'bg-[#fff]' : 'bg-[#32334B]',
+        ]"
+       >
+         <div class="grid grid-cols-2 gap-2">
+           <div class="flex flex-col">
+             <p
+                 :class="[
+                'text-[14px] leading-[16px] font-bold font-gilroy',
+                userStore.colorScheme === 'light'
+                  ? 'text-primary_dark_color'
+                  : 'text-[#fff]',
+              ]"
+             >
+               0
+             </p>
+             <p
+                 :class="[
+                'text-[14px] leading-[16px] font-medium font-gilroy',
+                userStore.colorScheme === 'light'
+                  ? 'text-[#737E87]'
+                  : 'text-text_light_color',
+              ]"
+             >
+               Перешло по ссылке
+             </p>
+           </div>
+           <div class="flex flex-col">
+             <p
+                 :class="[
+                'text-[14px] leading-[16px] font-bold font-gilroy',
+                userStore.colorScheme === 'light'
+                  ? 'text-primary_dark_color'
+                  : 'text-[#fff]',
+              ]"
+             >
+               0
+             </p>
+             <p
+                 :class="[
+                'text-[14px] leading-[16px] font-medium font-gilroy',
+                userStore.colorScheme === 'light'
+                  ? 'text-[#737E87]'
+                  : 'text-text_light_color',
+              ]"
+             >
+               Участвует в розыгрыше
+             </p>
+           </div>
+         </div>
+       </div>
+     </main>
     </div>
   </div>
 </template>
@@ -78,11 +153,17 @@
 import { useUserStore } from "@/stores/useUserStore.js";
 import Button from "@/components/Button/Button.vue";
 const userStore = useUserStore();
-const emit = defineEmits(['close']);
-
+const emit = defineEmits(["close"]);
+import clipboard from "@/utils/clipboard";
+import { ref } from "vue";
+const link = ref("https://t.me/LotoClubBot/app?startapp=L2cb4c0");
 const closeDialog = () => {
-  emit('close');
-}
+  emit("close");
+};
+const copy = () => {
+  clipboard(link.value);
+  link.value = "скопирован ✅🔗";
+};
 </script>
 
 <style>
