1753da6e7e05f8c416cdbaff4abd684bf5e4685d
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-09-20 16:08:49 +0500

    add min height

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -29,7 +29,7 @@
         :class="[
             colorScheme === 'light' &&  (userStore.colorScheme === 'light' ? 'bg-[#D0D9FA] text-[#1B1C37]' : 'bg-[#262744] text-[#FFFFFF]'),
             colorScheme === 'orange' &&  (userStore.colorScheme === 'light' ? 'bg-orange_color text-white' : 'bg-orange_color text-white'),
-            'font-gilroy font-medium text-center cursor-pointer transition text-[16px] flex justify-center items-center h-[50px] rounded-[6px] w-full max-w-lg px-2 active:opacity-75'
+            'font-gilroy font-medium text-center cursor-pointer transition text-[16px] flex justify-center items-center min-h-[50px] rounded-[6px] w-full max-w-lg px-2 active:opacity-75'
         ]"
     >
         {{ title }}
