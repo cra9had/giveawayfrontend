b235b488ccae6fba3ade1710b17d2c427d4a82df
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-09-20 09:34:20 +0500

    remove padding

--- src/views/Condition/ToDoCondition.vue
+++ src/views/Condition/ToDoCondition.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col h-full">
-  <div class="p-4 flex flex-col gap-2">
+  <div class="flex flex-col gap-2">
     <Hero
         title="Вы не выполнили условия розыгрыша!"
         image="error"
